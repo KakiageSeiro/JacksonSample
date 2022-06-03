720d60ee53a78b2d12a87e071f76137f302f370f
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2022-06-04 08:37:42 +0900

    SpringInitializrで作った直後

---
+++ src/main/java/com/kakiageseiro/JacksonSample/JacksonSampleApplication.java
@@ -0,0 +1,13 @@
+package com.kakiageseiro.JacksonSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakiageseiro/JacksonSample/JacksonSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakiageseiro.JacksonSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
